5029702f375bb6a5487212b78067991b61526961
Augusto <augstarachrafael@gmail.com>
2020-12-04 21:48:12 -0300

    filter by product name

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,13 +12,29 @@ import { Picker } from '@react-native-picker/picker';
 import { FlatList } from 'react-native-gesture-handler';
 import styled, { css } from 'styled-components/native';
 import ProductCard from '../components/ProductCard';
-import * as data from '../../products.json';
+// import * as data from '../../products.json';
 import { globalStyles } from '../Styles';
 import { CartContext } from '../contexts/CartContext';
 
 const OrderByMenu = styled(Picker)`
   background-color: white;
   elevation: 3;
+  margin-left: 10px;
+`;
+
+const HeaderListContainer = styled.View`
+  display: flex;
+  flex-flow: row;
+  margin-bottom: 10px;
+`;
+
+const TextInputFilter = styled.TextInput`
+  height: 50px;
+  elevation: 3;
+  border-radius: 10px;
+  background-color: white;
+  padding: 0 10px;
+  flex: 1;
 `;
 
 const ArrowDownIcon = styled(Icon)`
@@ -42,12 +58,28 @@ const DeleteBadge = styled.View`
   top: -5px;
 `;
 
-const FiltersContainer = styled.View`
-  padding: 20px 20px 0;
+const EmptyCartText = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  color: #979797;
+`;
+const EmptyCartIcon = styled(Icon)`
+  text-align: center;
 `;
 
+const EmptyProductList = () => {
+  return (
+    <SafeAreaView>
+      <EmptyCartIcon color="#979797" name="frowno" size={100} />
+      <EmptyCartText>Nenhum Jogo Encontrado</EmptyCartText>
+    </SafeAreaView>
+  );
+};
+
 const Home = ({ navigation }) => {
-  const products = [...data.default];
+  const productsMock = require('../../products.json');
+  const [products, setProducts] = useState(productsMock);
   const [cartState] = useContext(CartContext);
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -71,78 +103,94 @@ const Home = ({ navigation }) => {
     });
   }, [navigation, cartState]);
 
-  // const [productNameFilter, setProductNameFilter] = useState('');
+  const [productNameFilter, setProductNameFilter] = useState('');
   const [filteredProducts, setFilteredProducts] = useState([]);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     const patternFilter = new RegExp(productNameFilter, 'i');
-  //     const copyProducts = [...products];
-  //     const filtredArray = copyProducts.filter(
-  //       (product) => !productNameFilter || product.name.match(patternFilter),
-  //     );
-  //     setFilteredProducts(filtredArray);
-  //   }, 300);
-  //   return () => clearTimeout(timer);
-  // }, [productNameFilter]);
-
-  const [orderProductsBy, setOrderProducts] = useState(null);
   useEffect(() => {
-    if (orderProductsBy === null) {
-      setFilteredProducts([...products]);
-    } else if (orderProductsBy === 'price') {
-      const copyArrProducts = [...filteredProducts];
-      const sortedArr = copyArrProducts.sort(
+    const timer = setTimeout(() => {
+      const patternFilter = new RegExp(productNameFilter, 'i');
+      const copyProducts = [...products];
+      const filtredArray = copyProducts.filter(
+        (product) => !productNameFilter || product.name.match(patternFilter),
+      );
+      setFilteredProducts(filtredArray);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [productNameFilter]);
+
+  function orderProducts(type) {
+    const copyFilteredProducts = [...filteredProducts];
+    const copyProducts = [...products];
+    let sortedFiltered = [];
+    let sortedProducts = [];
+    if (type === null) {
+      setFilteredProducts(productsMock);
+      setProducts(productsMock);
+      return;
+    }
+    if (type === 'price') {
+      sortedFiltered = copyFilteredProducts.sort(
         (itemA, itemB) => parseFloat(itemA.price) - parseFloat(itemB.price),
       );
-      setFilteredProducts(sortedArr);
-    } else if (orderProductsBy === 'popularity') {
-      const copyArrProducts = [...filteredProducts];
-      const sortedArr = copyArrProducts.sort(
+      sortedProducts = copyProducts.sort(
+        (itemA, itemB) => parseFloat(itemA.price) - parseFloat(itemB.price),
+      );
+    } else if (type === 'popularity') {
+      sortedFiltered = copyFilteredProducts.sort(
         (itemA, itemB) => parseFloat(itemB.score) - parseFloat(itemA.score),
       );
-      setFilteredProducts(sortedArr);
+      sortedProducts = copyProducts.sort(
+        (itemA, itemB) => parseFloat(itemB.score) - parseFloat(itemA.score),
+      );
     } else {
-      const copyArrProducts = [...filteredProducts];
-      const sortedArr = copyArrProducts.sort(
+      sortedFiltered = copyFilteredProducts.sort(
         (itemA, itemB) => itemA.name > itemB.name,
       );
-      setFilteredProducts(sortedArr);
+      sortedProducts = copyProducts.sort(
+        (itemA, itemB) => itemA.name > itemB.name,
+      );
     }
+    setFilteredProducts(sortedFiltered);
+    setProducts(sortedProducts);
+  }
+  const [orderProductsBy, setOrderProducts] = useState(null);
+  useEffect(() => {
+    orderProducts(orderProductsBy);
   }, [orderProductsBy]);
 
   return (
-    <View>
-      {/* <Text>
-        <CartIcon />
-      </Text> */}
-      <FiltersContainer>
-        {/* <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(text) => setProductNameFilter(text)}
-          placeholder="Nome do Produto..."
-          multiline={false}
-          value={productNameFilter}
-        /> */}
-        <View>
-          <OrderByMenu
-            selectedValue={orderProductsBy}
-            onValueChange={(value) => setOrderProducts(value)}
-          >
-            <Picker.Item label="Selecione um filtro" value={null} />
-            <Picker.Item label="Preço" value="price" />
-            <Picker.Item label="Popularidade" value="popularity" />
-            <Picker.Item label="A -> Z" value="alphabetically" />
-          </OrderByMenu>
-          <ArrowDownIcon name="down" size={30} />
-        </View>
-      </FiltersContainer>
+    <View style={{ flex: 1 }}>
       {filteredProducts && (
         <FlatList
           data={filteredProducts}
           renderItem={({ item }) => <ProductCard product={item} />}
+          ListHeaderComponent={
+            <HeaderListContainer>
+              <TextInputFilter
+                onChangeText={(text) => setProductNameFilter(text)}
+                placeholder="Nome do Produto..."
+                multiline={false}
+                value={productNameFilter}
+              />
+              <View style={{ flex: 1 }}>
+                <OrderByMenu
+                  style={{ borderRadius: 10 }}
+                  selectedValue={orderProductsBy}
+                  onValueChange={(value) => setOrderProducts(value)}
+                >
+                  <Picker.Item label="Ordernar por..." value={null} />
+                  <Picker.Item label="Preço" value="price" />
+                  <Picker.Item label="Popularidade" value="popularity" />
+                  <Picker.Item label="A -> Z" value="alphabetically" />
+                </OrderByMenu>
+                <ArrowDownIcon name="down" size={30} />
+              </View>
+            </HeaderListContainer>
+          }
+          ListEmptyComponent={<EmptyProductList />}
           keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={{ paddingBottom: 100, paddingHorizontal: 20 }}
+          extraData={filteredProducts}
+          contentContainerStyle={{ padding: 20 }}
         />
       )}
     </View>
